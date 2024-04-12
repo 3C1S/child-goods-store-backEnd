7eac38df1bef6cd9c2ee81a3c7c592045c182589
Kim-Min-Gyeong <kimmin4051@naver.com>
2024-04-12 22:44:12 +0900

    Feat(Tag): 태그 검색

--- src/main/java/C1S/childgoodsstore/tag/controller/TagController.java
+++ src/main/java/C1S/childgoodsstore/tag/controller/TagController.java
@@ -1,10 +1,25 @@
 package C1S.childgoodsstore.tag.controller;
 
+import C1S.childgoodsstore.tag.dto.KeywordDto;
+import C1S.childgoodsstore.tag.service.TagService;
+import C1S.childgoodsstore.util.ApiResponse;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TagController {
 
+    private final TagService tagService;
+
+    @GetMapping("/tag")
+    public ResponseEntity<ApiResponse<List<String>>> getTag(@RequestBody KeywordDto keyword){
+        return ResponseEntity.ok(ApiResponse.success(tagService.getTag(keyword.getKeyword())));
+    }
 }
--- src/main/java/C1S/childgoodsstore/tag/repository/TagRepository.java
+++ src/main/java/C1S/childgoodsstore/tag/repository/TagRepository.java
@@ -1,5 +1,15 @@
 package C1S.childgoodsstore.tag.repository;
 
-public interface TagRepository {
+import C1S.childgoodsstore.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    @Query("SELECT t.tag FROM Tag t WHERE t.tag LIKE :keyword%")
+    List<String> findByTagStartingWith(@Param("keyword") String keyword);
 
 }
