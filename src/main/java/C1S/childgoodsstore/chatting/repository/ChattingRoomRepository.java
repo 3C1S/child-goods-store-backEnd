82a3687b52b3879b7e735a594d67a775d75e2a75
sujin11-kim <su091876@gmail.com>
2024-04-16 00:54:32 +0900

    Feat: 채팅방 생성 API(304) 추가
    중고거래 상품 또는 공동 구매 상품에 대한 채팅방을 생성할 수 있습니다. 각 상품 카테고리에 따라 적절한 채팅방이 생성됩니다.

--- src/main/java/C1S/childgoodsstore/chatting/repository/ChattingRoomRepository.java
+++ src/main/java/C1S/childgoodsstore/chatting/repository/ChattingRoomRepository.java
@@ -1,10 +1,13 @@
 package C1S.childgoodsstore.chatting.repository;
 
 import C1S.childgoodsstore.entity.ChattingRoom;
+import C1S.childgoodsstore.entity.Together;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
     // 상품 ID로 채팅방 검색
     List<ChattingRoom> findByProductProductId(Long productId);
+
+    ChattingRoom findByTogether(Together together);
 }
