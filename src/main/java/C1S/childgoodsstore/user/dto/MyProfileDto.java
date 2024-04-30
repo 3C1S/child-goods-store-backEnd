9ad6a8f1b42a67a428d6e44e2d0fc4614991d877
kyeongjeong <ariel0918@kw.ac.kr>
2024-05-01 02:10:47 +0900

    Chore: 프로필 조회에 createdAt 추가

--- src/main/java/C1S/childgoodsstore/user/dto/MyProfileDto.java
+++ src/main/java/C1S/childgoodsstore/user/dto/MyProfileDto.java
@@ -3,6 +3,8 @@ package C1S.childgoodsstore.user.dto;
 import C1S.childgoodsstore.entity.User;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
@@ -17,12 +19,14 @@ public class MyProfileDto {
     private int followNum;
     private int followingNum;
     private double averageStars;
+    private LocalDateTime createdAt;
 
     public MyProfileDto(User user, int followingNum, int followNum, double averageStars) {
         this.userId = user.getUserId();
         this.nickName = user.getNickName();
         this.introduce = user.getIntroduce();
         this.profileImg = user.getProfileImg();
+        this.createdAt = user.getCreatedAt();
         this.followNum = followNum;
         this.followingNum = followingNum;
         this.averageStars = averageStars;
--- src/main/java/C1S/childgoodsstore/user/dto/UserProfileDto.java
+++ src/main/java/C1S/childgoodsstore/user/dto/UserProfileDto.java
@@ -3,6 +3,8 @@ package C1S.childgoodsstore.user.dto;
 import C1S.childgoodsstore.entity.User;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
@@ -18,12 +20,14 @@ public class UserProfileDto {
     private int followNum;
     private int followingNum;
     private double averageStars;
+    private LocalDateTime createdAt;
 
     public UserProfileDto(User user, boolean isFollowed, int followingNum, int followNum, double averageStars) {
         this.userId = user.getUserId();
         this.nickName = user.getNickName();
         this.introduce = user.getIntroduce();
         this.profileImg = user.getProfileImg();
+        this.createdAt = user.getCreatedAt();
         this.isFollowed = isFollowed;
         this.followNum = followNum;
         this.followingNum = followingNum;
