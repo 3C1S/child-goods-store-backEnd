6dfc481f1ff48e8c20103f9b7a5923a3819f329f
sujin11-kim <su091876@gmail.com>
2024-05-02 15:16:13 +0900

    Fix: 상품 상세 조회 API(106) 반환형 수정

--- src/main/java/C1S/childgoodsstore/product/dto/output/ProductDetailsDto.java
+++ src/main/java/C1S/childgoodsstore/product/dto/output/ProductDetailsDto.java
@@ -29,7 +29,7 @@ public class ProductDetailsDto {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private List<String> tag;
-    private List<String> productImages;
+    private List<String> productImage;
     private boolean productHeart;
 
     // UserDto 내부 클래스
@@ -52,7 +52,7 @@ public class ProductDetailsDto {
     public ProductDetailsDto(Long productId, UserDto user, String productName, int price, String content,
                       MAIN_CATEGORY mainCategory, SUB_CATEGORY subCategory, AGE age, PRODUCT_STATE productState,
                       PRODUCT_SALE_STATUS state, LocalDateTime createdAt, LocalDateTime updatedAt,
-                      List<String> tag, List<String> productImages, boolean productHeart) {
+                      List<String> tag, List<String> productImage, boolean productHeart) {
         this.productId = productId;
         this.user = user;
         this.productName = productName;
@@ -66,7 +66,7 @@ public class ProductDetailsDto {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.tag = tag;
-        this.productImages = productImages;
+        this.productImage = productImage;
         this.productHeart = productHeart;
     }
 
