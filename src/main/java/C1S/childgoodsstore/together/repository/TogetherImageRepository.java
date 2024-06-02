2d3f04c5ea8fe802280b650ed4fcdc9da71ab13a
sujin11-kim <su091876@gmail.com>
2024-06-03 05:45:22 +0900

    Feat: TogetherImage 테이블 추가

---
+++ src/main/java/C1S/childgoodsstore/together/repository/TogetherImageRepository.java
@@ -0,0 +1,9 @@
+package C1S.childgoodsstore.together.repository;
+
+import C1S.childgoodsstore.entity.TogetherImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TogetherImageRepository extends JpaRepository<TogetherImage, Long> {
+
+}
